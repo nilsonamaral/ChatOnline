da34ea027e00fbc5de1bf00306bc10b44853f28e
nilsonamaral <nilsonamaral7@gmail.com>
2024-03-27 21:00:30 -0300

    change terminal

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -81,7 +81,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chatonline-eqeb.onrender.com")
     websocket.onmessage = processMessage
 }
 
